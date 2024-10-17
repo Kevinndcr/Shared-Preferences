1a77d1ed985c984d673bc6fce81dd38037f47567
Kevinndcr <kecordobari@est.utn.ac.cr>
2024-10-17 14:41:00 -0600

    first commit

---
+++ app/src/main/java/com/kevin/a09_sharedpreferences/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.kevin.a09_sharedpreferences
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import android.content.Context
+import android.content.Intent
+
+class MainActivity : AppCompatActivity() {
+
+
+    var email: String? = null
+    var contra: String? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val prefe = getSharedPreferences("appData", Context.MODE_PRIVATE)
+        email = prefe.getString("email","")
+        contra = prefe.getString("contra","")
+
+        if(email.toString().trim { it <= ' ' }.isEmpty()){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+    }
+}
